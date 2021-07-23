d6c07a9f2aecb96a5ada4eab9f045a5d583cdfd3
fullyallocated <fullyallocated@gmail.com>
2021-07-23 07:49:46 -0400

    compile successful, extend OZ's ERC20 successfully in VaultShares

--- contracts/states/interfaces/VaultSharesV1.sol
+++ contracts/states/interfaces/VaultSharesV1.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-interface STATE_VaultShares is IERC20Metadata {
+interface STATE_VaultShares {
     // state changes (writes)
     function issueShares(address depositor_, uint256 amount_) external;
     function burnShares(uint256 amount_) external;
