7854d71db1a769e6623b7bbf3835191bbb606aa3
fullyallocated <fullyallocated@gmail.com>
2021-08-24 02:30:05 -0400

    add additional files

---
+++ contracts/os/Members/Members.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../DefaultOS.sol";
+import "../Members/_Staking.sol";
+import "../Token/Token.sol";
+import "hardhat/console.sol";
+
+contract def_MembersInstaller is DefaultOSModuleInstaller("MBR") {
+    string public moduleName = "Default Members";
+
+    function install(DefaultOS os_) external override returns (address) {
+        def_Members members = new def_Members(os_);
+        members.transferOwnership(address(os_)); 
+        return address(members);
+    }
+}
+
+contract def_Members is Staking, DefaultOSModule {
+
+    // Module Configuration
+    def_Token private _Token;
+
+    constructor(DefaultOS os_) DefaultOSModule(os_) {
+        _Token = def_Token(_OS.getModule("TKN"));
+    }
+    
+
+    // Emitted events for this module
+    event MemberRegistered(address member, uint16 currentEpoch);
+    event TokensStaked(address member, uint256 amount, uint16 lockDuration, uint16 currentEpoch);
+    event TokensUnstaked(address member, uint256 amount, uint16 lockDuration, uint16 currentEpoch);
+    event EndorsementGiven(address fromMember, address toMember, uint256 endorsementsGiven, uint16 currentEpoch);
+    event EndorsementWithdrawn(address fromMember, address toMember, uint256 endorsementsWithdrawn, uint16 currentEpoch);
+
+
+    // alias stuff
+    mapping(bytes32 => address) public getMemberForAlias;
+    mapping(address => bytes32) public getAliasForMember;
+
+    // endorsement logic for users
+    mapping(address => uint256) public totalEndorsementsAvailableToGive;
+    mapping(address => uint256) public totalEndorsementsGiven;
+    mapping(address => uint256) public totalEndorsementsReceived;
+    mapping(address => mapping(address => uint256)) public endorsementsGiven; // GIVER => mapping (RECEIVER => AMOUNT)
+    mapping(address => mapping(address => uint256)) public endorsementsReceived; // RECEIVER => mapping (GIVER => AMOUNT)
+
+
+    // endorsement multipliers for staking, based on duration. Increase scale quadratically to incentivize long term holder
+    function _getMultiplierForStakingDuration(uint16 lockDuration_) private pure returns (uint256 multiplier) {
+        if (lockDuration_ < 50 ) { return 0; }
+        else if (lockDuration_ >= 50  && lockDuration_ < 100) { return 1; }
+        else if (lockDuration_ >= 100 && lockDuration_ < 150) { return 3; }
+        else if (lockDuration_ >= 150 && lockDuration_ < 200) { return 6; }
+        else if (lockDuration_ >= 200) { return 10; }
+    }
+
+
+
+    // **********************************************************************
+    //                     MINT ENDORSEMENTS TO GIVE
+    // **********************************************************************
+
+    function setAlias(bytes32 alias_) external {
+        // make sure the alias space is empty 
+        require (getMemberForAlias[alias_] == address(0), "def_Members | (): Members | setAlias(): alias is already taken");
+        getAliasForMember[msg.sender] = alias_;
+        getMemberForAlias[alias_] = msg.sender;
+    }
+
+
+
+    // **********************************************************************
+    //                     MINT ENDORSEMENTS TO GIVE
+    // **********************************************************************
+
+    function mintEndorsements(uint16 lockDuration_, uint256 amount_) external {
+        require (amount_ > 0, "def_Members | (): def_Members | mintEndorsements() : member must stake more than 0 tokens");
+        require (lockDuration_ >= 50, "def_Members | (): def_Members | mintEndorsements(): member must stake for at least 50 epochs");
+        
+        uint16 expiryEpoch = _OS.currentEpoch() + lockDuration_;
+
+        // get the adjusted amount of endorsements based on staking duration
+        totalEndorsementsAvailableToGive[msg.sender] += amount_ * _getMultiplierForStakingDuration(lockDuration_);
+
+        // register the stake and transfer tokens
+        _registerNewStake(expiryEpoch, lockDuration_, amount_);
+        _Token.transferFrom(msg.sender, address(this), amount_);
+
+        emit TokensStaked(msg.sender, amount_, lockDuration_, _OS.currentEpoch());
+    }
+
+
+
+    // **********************************************************************
+    //                  RECLAIM TOKENS FROM EXPIRED STAKES
+    // **********************************************************************
+
+    function reclaimTokens() external {
+
+        // get the stakes for the caller
+        (uint16 lockDuration, uint16 expiryEpoch, uint256 amountStaked) = _dequeueStake();
+
+        // User should withdraw the necessary endorsements prior to unstaking
+        require (totalEndorsementsAvailableToGive[msg.sender] - amountStaked >= totalEndorsementsGiven[msg.sender], "def_Members | reclaimTokens(): Not enough endorsements remaining after unstaking");
+
+        // The current Epoch should be higher than the expiry epoch
+        require (_OS.currentEpoch() >= expiryEpoch, "def_Members | reclaimTokens(): No expired stakes available for withdraw");
+
+        // get the adjusted tokens to reclaim based on the lock duration and transfer the tokens back to the member
+        totalEndorsementsAvailableToGive[msg.sender] -= amountStaked * _getMultiplierForStakingDuration(lockDuration);
+        _Token.transfer(msg.sender, amountStaked);
+
+        emit TokensUnstaked(msg.sender, amountStaked, lockDuration, _OS.currentEpoch());
+    }
+
+    
+
+    // **********************************************************************
+    //                        ENDORSE ANOTHER MEMBER
+    // **********************************************************************
+
+    function endorseMember(address targetMember_, uint256 amount_) external {
+
+        // ensure that the member has enough endorsements available
+        require (totalEndorsementsGiven[msg.sender] + amount_ <= totalEndorsementsAvailableToGive[msg.sender], "def_Members | endorseMember(): Member does not have available endorsements to give");
+
+        // increment the applicable states for the giver
+        totalEndorsementsGiven[msg.sender] += amount_;
+        endorsementsGiven[msg.sender][targetMember_] += amount_;
+
+        // increment the applicable states for the receiver
+        totalEndorsementsReceived[targetMember_] += amount_;
+        endorsementsReceived[targetMember_][msg.sender] += amount_;
+
+        emit EndorsementGiven(msg.sender, targetMember_, amount_, _OS.currentEpoch());
+    }
+
+
+
+    // **********************************************************************
+    //               WITHDRAW ENDORSEMENTS FROM ANOTHER MEMBER
+    // **********************************************************************
+
+    function withdrawEndorsementFrom(address targetMember_, uint256 amount_) external {
+
+        // ensure that the member has enough endorsements to withdraw
+        require(endorsementsGiven[msg.sender][targetMember_] >= amount_, "def_Members | withdrawEndorsementFrom(): Not enough endorsements to withdraw");
+
+        // decrement the applicable states for the giver
+        totalEndorsementsGiven[msg.sender] -= amount_;
+        totalEndorsementsReceived[targetMember_] -= amount_;
+
+        // decrement the applicable states for the giver
+        endorsementsGiven[msg.sender][targetMember_] -= amount_;
+        endorsementsReceived[targetMember_][msg.sender ] -= amount_;
+
+        emit EndorsementWithdrawn(msg.sender, targetMember_, amount_, _OS.currentEpoch());
+    }
+}
---
+++ contracts/os/Members/_Staking.sol
@@ -0,0 +1,205 @@
+// Quick Notes
+//
+// Stakes are not ERC20 tokens—they are simply locked in the contract. No ERC20/composability
+// Around the staked tokens so as to not reduce the opportunity cost of the liquidity preference
+// e.g. staking DEF is less of a commitment if someone can swap their sDEF for DEF/USDC immediately.
+// And less confusing for the user when participating in governance/selling their tokens (see vBNT/BNT).
+//
+// Implementation
+//
+// The way we implement staking is with a sorted doubly linked list of "Stakes", or individual staking
+// blocks of tokens that are locked for some amount of time. The list is sorted by FIRST expiration date 
+// (basically the FIRST staking block to be unlocked). This makes it easy to insert arbitrary length stakes,
+// allowing us to experiment with things like multipliers for longer stakes in future design iterations.
+// (shoutout to https://curve.fi/ for the mechanism inspiration).
+//
+// ADDED NOTES (8/17/21): Originally we used the expiry epoch as the primary id to sort each stake, but this was changed
+// due to the fact that different lock durations generate different multipliers for endorsements, so each
+// individual stake needs to be recorded (even if two stakes expire on the same epoch). This creates some constraints
+// because the length of total stakes increases.
+// 
+//
+// Concerns/Improvements
+//
+// This contract has some issues, namely around the length of the list + traversals in staking and unstaking logic.
+// There is a storage write for every stake dequeued, which can get expensive in the unstaking process.
+// These inefficiencies probably create an upper bound around the max number of Stakes stored due to gas limits, so
+// a top dev priority should be 1) testing the bounds of the contract for staking and 2) creating safety barriers around them.
+// In the beginning I think things are okay because the only real failure points are staking with low duration after a lot of high
+// duration staking (e.g. inserting a 1 year stake after 100 2 year stakes) and unstaking a large amount of tokens at once after a lot
+// of stakes have expired.
+//
+// ADDED NOTES (8/17/21): Another potential (but slight) issue with this implementation arises when it comes to unstaking.
+// In the current implementation, unstaking happens by dequeuing the first stake in the list (since it has the earliest expiry)
+// However, because different stakes have different endorsement multipliers, a user may in theory want to withdraw/unstake
+// a later stake in the linked list while preserving the first one (especially if it has a high multiplier). However,
+// I think this is a very nuanced edge case that I cannot forsee causing major issues in the immediate future, but we'll see.
+//
+// - fully
+//
+
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0;
+
+import "../DefaultOS.sol";
+import "hardhat/console.sol";
+
+contract Staking { // owned by (and called by) the Membership Module
+
+    // pack the struct variables--order declaration matters!
+    // single stake object for a list of stakes
+    struct Stake {
+        uint16 expiryEpoch; // epoch when this batch of staked tokens expire => (lock duration + epoch staked). used as the comparison for the list sort.
+        uint16 lockDuration; // used for endorsement multiplier logic around different lock durations 
+        uint32 prevStakeId; // id of prev stake to expire in sorted linked list of staking blocks
+        uint32 nextStakeId; // id of next stake to expire in linked list of staking blocks
+        uint256 amountStaked; // DEF tokens amount staked in this batch
+    }
+
+    // sorted doubly linked list of stakes for a given user
+    struct StakesList {
+        uint8 numStakes; // number of Stakes for the user
+        uint32 FIRST; // the FIRST stake to expire
+        uint32 LAST; // the LAST stake to expire
+        uint256 totalTokensStaked;// total amount of staked tokens for the user
+        mapping(uint32 => Stake) getStakeForId; // key is a composite id consisting of expiryEpoch + lockDuration
+    }
+
+    // get the list of stakes for a given member;
+    mapping(address => StakesList) public getStakesForMember;
+
+    // used to get the stake object from the mapping
+    function _packStakeId(uint16 expiryEpoch_, uint16 lockDuration_) internal pure returns(uint32 stakeId) {
+        // shift expiry epoch 16 bits to the left and append the 16bit lock duration at the end to create a composite ID
+        return (uint32(expiryEpoch_) << 16) | uint32(lockDuration_);
+    }
+
+    // deconstruct the composite ID of the stake to get the expiry epoch and lock duration
+    function _unpackStakeId(uint32 stakeId_) internal pure returns(uint16 lockDuration, uint16 expiryEpoch) {
+        expiryEpoch = uint16(stakeId_ >> 16);
+        lockDuration = uint16(stakeId_);
+        return (expiryEpoch, lockDuration);
+    }
+
+    // Push the stake to the end of the linked list
+    function _pushStake(uint16 expiryEpoch_, uint16 lockDuration_, uint256 amount_) internal {
+        StakesList storage stakes = getStakesForMember[msg.sender];
+
+        // create and map new Stake
+        uint32 newStakeId = _packStakeId(expiryEpoch_, lockDuration_);
+        stakes.getStakeForId[newStakeId] = Stake(expiryEpoch_, lockDuration_, stakes.LAST, 0, amount_);
+
+        // if stake is the only item in the list, make it the new FIRST as well.
+        if (stakes.numStakes == 0) { 
+            stakes.FIRST = newStakeId;
+
+        // otherwise, find the last stake and set its next stake to the new stake.
+        } else {
+            Stake storage curLAST = stakes.getStakeForId[stakes.LAST];
+            curLAST.nextStakeId = newStakeId;
+        }
+
+        // set the current stake to be the new LAST item in the linked list & update the appropriate state variables
+        stakes.LAST = newStakeId;
+        stakes.totalTokensStaked += amount_;    
+        stakes.numStakes++;
+    }
+
+    function _insertStakeBefore(uint32 insertedBeforeStakeId_, uint16 expiryEpoch_, uint16 lockDuration_, uint256 amount_) internal {
+        StakesList storage stakes = getStakesForMember[msg.sender];
+
+        // ensure there are existing stakes before inserting new stake before anything
+        require (stakes.numStakes != 0, "Staking.sol: cannot insertStakeBefore() in empty list of Stakes");
+        
+        // get the object of the stake expiring after the new stake
+        Stake memory afterNewStake = stakes.getStakeForId[insertedBeforeStakeId_];
+
+        // create, configure, and map new Stake
+        uint32 newStakeId = _packStakeId(expiryEpoch_, lockDuration_);
+        stakes.getStakeForId[newStakeId] = Stake(expiryEpoch_, lockDuration_, afterNewStake.prevStakeId, insertedBeforeStakeId_, amount_);
+
+        // If the old stake was the FIRST, make the inserted stake the new FIRST.
+        if (insertedBeforeStakeId_ == stakes.FIRST) {
+            stakes.FIRST = newStakeId;
+
+        // Otherwise, change the "next" pointer for the stake expiring before the new stake
+        } else {
+            Stake memory beforeNewStake = stakes.getStakeForId[afterNewStake.prevStakeId];
+            beforeNewStake.nextStakeId = newStakeId;
+            stakes.getStakeForId[afterNewStake.prevStakeId] = beforeNewStake;
+        }
+
+        // Set the next stake's prev pointer to the newly inserted Stake.
+        afterNewStake.prevStakeId = newStakeId;
+        stakes.getStakeForId[insertedBeforeStakeId_] = afterNewStake;
+
+        // update the appropriate state variables
+        stakes.totalTokensStaked += amount_;    
+        stakes.numStakes++;
+    }
+
+    // When registering new stakes, do a sorted insert into the doubly linked list based on expiryEpochn
+    function _registerNewStake(uint16 expiryEpoch_, uint16 lockDuration_, uint256 amount_) internal {
+        StakesList storage stakes = getStakesForMember[msg.sender];
+
+        uint32 newStakeId = _packStakeId(expiryEpoch_, lockDuration_);
+        Stake memory newStake = stakes.getStakeForId[newStakeId];
+        Stake memory lastStake = stakes.getStakeForId[stakes.LAST];
+
+        // If a stake exists for the expiryEpoch + lockDuration, then
+        // user wants to re-up (so add amount to existing stake) 
+        if (newStake.expiryEpoch != 0) {
+            newStake.amountStaked += amount_;
+            stakes.getStakeForId[newStakeId] = newStake;
+            stakes.totalTokensStaked += amount_;
+            // then push the stake to the end of list
+
+        // If no stakes exist, or if the current stake's expiry Epoch is greater than or equal to the last expiry,
+        } else if (stakes.numStakes == 0 || expiryEpoch_ >= lastStake.expiryEpoch) { 
+            _pushStake(expiryEpoch_, lockDuration_, amount_);
+
+        // Otherwise, loop through the linked list starting from the end until the appropriate spot for the 
+        // new stake is found, and insert the stake in the correct position.
+        } else {
+            Stake memory prevStake = stakes.getStakeForId[lastStake.prevStakeId];
+            while (prevStake.expiryEpoch != 0 && prevStake.expiryEpoch > expiryEpoch_) {
+                lastStake = prevStake;
+                prevStake = stakes.getStakeForId[prevStake.prevStakeId];
+            }
+
+            uint32 existingStakeId = _packStakeId(lastStake.expiryEpoch, lastStake.lockDuration);
+            _insertStakeBefore(existingStakeId, expiryEpoch_, lockDuration_, amount_);
+        }
+    }
+
+    function _dequeueStake() internal returns (uint16 lockDuration_, uint16 expiryEpoch_, uint256 amountStaked_) {
+        StakesList storage stakes = getStakesForMember[msg.sender];
+
+        // Ensure stakes exist before dequeueing
+        require (stakes.numStakes > 0, "cannot dequeue empty stakes list");
+
+        Stake memory firstStake = stakes.getStakeForId[stakes.FIRST];
+        
+        // reset stake at FIRST stakeId
+        stakes.getStakeForId[stakes.FIRST] = Stake(0, 0, 0, 0, 0);
+
+        if (stakes.numStakes == 1) {
+            stakes.FIRST = 0;
+            stakes.LAST = 0;
+            
+        } else {
+            // set the nextStake of the dequeued stake to be the first stake in the list        
+            Stake memory nextStake = stakes.getStakeForId[firstStake.nextStakeId];
+            nextStake.prevStakeId = 0;
+            stakes.getStakeForId[firstStake.nextStakeId] = nextStake;
+            stakes.FIRST = firstStake.nextStakeId;
+        }
+
+        // update the appropriate state variables
+        stakes.totalTokensStaked -= firstStake.amountStaked;
+        stakes.numStakes--;
+
+        return (firstStake.lockDuration, firstStake.expiryEpoch, firstStake.amountStaked);
+    }
+}
